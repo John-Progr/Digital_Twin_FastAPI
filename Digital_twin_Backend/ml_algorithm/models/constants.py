bd20f2bf425f530ae81ce0a6db07d8439ed2bfd0
John Matsoukas <62017772+John-Progr@users.noreply.github.com>
2025-02-21 20:01:35 +0200

    Delete Digital_twin_Backend/ml_algorithm directory

--- Digital_twin_Backend/ml_algorithm/models/constants.py
+++
@@ -1,9 +0,0 @@
-class ModelConstants:
-    RCL_COEFFICIENTS = {
-        'intercept': -5.939,
-        'hello': 3.225,
-        'tc': 0.565,
-        'window': 0.142,
-        'avg_neighbors': -0.168,
-        'std_neighbors': 0.624
-    }
